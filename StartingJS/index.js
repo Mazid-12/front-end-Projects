7159afcbe68091edf98d85b57fa2854fcf14eb87
Mazid <mazidyerima2@gmail.com>
2026-06-08 21:12:19 +0000

    Today, I started learning Javascript. I learn how to print: console.log and how to show an alert window.alert()

---
+++ StartingJS/index.js
@@ -0,0 +1,9 @@
+/*console.log("Hello");
+console.log('I am building a website')
+
+window.alert('This is an alert')
+window.alert('I am building')*/
+
+document.getElementById("headline").textContent = 'Hello';
+ducument.getElementById('phrase').textContent = 'I like building'
+// this is a comment
